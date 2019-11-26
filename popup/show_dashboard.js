1fbf23199d7496389358e96bac2e973066095459
lucywalsh <lucyhannahwalsh@gmail.com>
2019-11-26 12:07:33 +0000

    boilerplate

---
+++ popup/show_dashboard.js
@@ -0,0 +1,5 @@
+function reportExecuteScriptError(error) {
+  document.querySelector("#popup-content").classList.add("hidden");
+  document.querySelector("#error-content").classList.remove("hidden");
+  console.error(`Failed to execute beastify content script: ${error.message}`);
+}
