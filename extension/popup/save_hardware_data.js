26720777105fcb30d28d781b25455e0d96dc9dff
Lucy Walsh <lucywalsh@Lucys-MBP.broadband>
2020-05-06 17:56:25 +0100

    updated alerts

--- extension/popup/save_hardware_data.js
+++ extension/popup/save_hardware_data.js
@@ -16,47 +16,56 @@ window.addEventListener('load',(event) => {
   })
 });
 */
+
+var alert_dict = {
+  "Firstparty-tracking":"First-party Tracking",
+  "Thirdparty-collection":"Third-party Collection",
+  "Targeted-ads","Targeted Advertising",
+  "Personalisation":"Site Personalisation",
+  "Thirdparty-tracking":"Third-party Tracking",
+  "Location":"Location Information",
+  "Financial":"Financial Information",
+  "Personal":"Personal Information",
+  "DoNotTrack":"Do Not Track Headers Ignored",
+  "Health":"Health Information"
+}
+
 document.getElementById("save_hardware").onclick = function save_hardware_data(){
   var hardware_settings = {};
 
-  var userprofiling_mode = document.getElementById("user-profiling_modedropdown");
+  var userprofiling_mode = document.getElementById("firstparty-tracking_modedropdown");
   if(userprofiling_mode != null){
-    hardware_settings['user-profiling'] = userprofiling_mode.value;
+    hardware_settings['firstparty-tracking'] = userprofiling_mode.value;
   }
 
-  var targetedadvertising_mode = document.getElementById("targeted-advertising_modedropdown");
+  var targetedadvertising_mode = document.getElementById("targeted-ads_modedropdown");
   if(targetedadvertising_mode != null){
-    hardware_settings['targeted-advertising'] = targetedadvertising_mode.value;
+    hardware_settings['targeted-ads'] = targetedadvertising_mode.value;
   }
 
-  var datasharing_mode = document.getElementById("data-sharing_modedropdown");
+  var datasharing_mode = document.getElementById("thirdparty-tracking_modedropdown");
   if(datasharing_mode != null){
-    hardware_settings['data-sharing'] = datasharing_mode.value;
+    hardware_settings['thirdparty-tracking'] = datasharing_mode.value;
   }
 
-  var datacollection_mode = document.getElementById("data-collection_modedropdown");
+  var datacollection_mode = document.getElementById("personalisation_modedropdown");
   if(datacollection_mode != null){
-    hardware_settings['data-collection'] = datacollection_mode.value;
+    hardware_settings['personlisation'] = datacollection_mode.value;
   }
 
-  var unencryptedcomms_mode = document.getElementById("unencrypted-comms_modedropdown");
+  var unencryptedcomms_mode = document.getElementById("thirdparty-tracking_modedropdown");
   if(unencryptedcomms_mode != null){
-    hardware_settings['unencrypted-comms'] = unencryptedcomms_mode.value;
+    hardware_settings['thirdparty-tracking'] = unencryptedcomms_mode.value;
   }
 
-  var accesstocomms_mode = document.getElementById("access-to-comms_modedropdown");
+  var accesstocomms_mode = document.getElementById("financial_modedropdown");
   if(accesstocomms_mode != null){
-    hardware_settings['access-to-comms'] = accesstocomms_mode.value;
+    hardware_settings['financial'] = accesstocomms_mode.value;
   }
 
-  var thirdpartytracking_mode = document.getElementById("thirdparty-tracking_modedropdown");
-  if(thirdpartytracking_mode != null){
-    hardware_settings['thirdparty-tracking'] = thirdpartytracking_mode.value;
-  }
-
-  var expecteduse_mode = document.getElementById("expected-use_modedropdown");
+  var expecteduse_mode = document.getElementById("donottrack_modedropdown");
   if(expecteduse_mode != null){
-    hardware_settings['expected-use'] = expecteduse_mode.value;
+    hardware_settings['donottrack'] = expecteduse_mode.value;
   }
 
   var location_mode = document.getElementById("location_modedropdown");
@@ -64,9 +73,14 @@ document.getElementById("save_hardware").onclick = function save_hardware_data()
     hardware_settings['location'] = location_mode.value;
   }
 
-  var browserfingerprinting_mode = document.getElementById("browser-fingerprinting_modedropdown");
+  var browserfingerprinting_mode = document.getElementById("health_modedropdown");
   if(browserfingerprinting_mode != null){
-    hardware_settings['browser-fingerprinting'] = browserfingerprinting_mode.value;
+    hardware_settings['health'] = browserfingerprinting_mode.value;
+  }
+
+  var browserfingerprinting_mode = document.getElementById("personal_modedropdown");
+  if(browserfingerprinting_mode != null){
+    hardware_settings['personal'] = browserfingerprinting_mode.value;
   }
 
   var temp = {};
